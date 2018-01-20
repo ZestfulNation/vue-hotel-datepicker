eeb0ddcff4f010bdc60966aad0cd402718488c40
Nick Hough <nickhough@users.noreply.github.com>
2018-01-20 12:13:44 -0700

    Made check-in/out label casing consistent

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -109,7 +109,7 @@ const defaulti18n = {
   nights: 'Nights',
   'day-names': ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
   'check-in': 'Check-in',
-  'check-out': 'Check-Out',
+  'check-out': 'Check-out',
   'month-names': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 };
 
