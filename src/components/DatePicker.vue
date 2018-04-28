f1359ecc7da40d79972706038b2231f3a274bada
chrisrenga <13778529+chrisrenga@users.noreply.github.com>
2018-04-28 07:04:04 +0100

    fixes horizontal scroll on mobile devices (#64)
    there is an issue that is causing a horizontal scroll once the datepicker is open on mobile (landscape view) and tablet (portrait view).
    
    this fixes the issue by adding the padding only on large screens

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -133,7 +133,7 @@ export default {
     },
     endingDateValue: {
       default: null,
-      type: Date 
+      type: Date
     },
     format: {
       default: 'YYYY-MM-DD',
@@ -749,7 +749,7 @@ $font-small: 14px;
       }
     }
 
-    @include device($tablet-up) {
+    @include device($desktop) {
       &:last-of-type {
         padding-right: 0;
         padding-left: 10px;
