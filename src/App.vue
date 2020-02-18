cd885b797b987fd94640b7d805f809debdc617f9
Matias Perrone <matias@globalinnovation.com.ar>
2020-02-18 15:22:50 -0300

    Pre-alpha version with priceByDate (a.k.a. *Add pricing per night* #176) PR incorporated. Some other development configuration files.

--- src/App.vue
+++ src/App.vue
@@ -1,203 +1,193 @@
 <template>
-  <div id="app" style="text-align: left; padding: 10vw">
-    <div>
+    <div id="app" style="text-align: left; padding: 10vw">
+        <div>
+            <div class="box">
+                <h3>Allow check-in and check-out on the same day</h3>
+                <DatePicker :minNights="0" />
+            </div>
+            <div class="box">
+                <h3>Allow selection of single day</h3>
+                <DatePicker :singleDaySelection="true" />
+            </div>
+            <div class="box">
+                <h3>Check in only on saturday and minimum stay of 10 nights</h3>
+                <DatePicker
+                    :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
+                    :enableCheckout="true"
+                    :minNights="10"
+                />
+            </div>
 
-      <div class="box">
-        <h3>Allow check-in and check-out on the same day</h3>
-        <DatePicker
-          :minNights="0"
-        />
-      </div>
-      <div class="box">
-        <h3>Allow selection of single day</h3>
-        <DatePicker
-          :singleDaySelection="true"
-        />
-      </div>
-      <div class="box">
-        <h3>Check in only on saturday and minimum stay of 10 nights</h3>
-        <DatePicker
-          :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
-          :enableCheckout="true"
-          :minNights="10"
-        />
-      </div>
+            <div class="box">
+                <h3>Check in and check-out only on saturday and maximum stay of 30 nights</h3>
+                <DatePicker
+                    :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
+                    :enableCheckout="true"
+                    :allowedRanges="[7,14,21,28]"
+                    :maxNights="30"
+                />
+            </div>
 
-      <div class="box">
-        <h3>Check in and check-out only on saturday and maximum stay of 30 nights</h3>
-        <DatePicker
-          :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
-          :enableCheckout="true"
-          :allowedRanges="[7,14,21,28]"
-          :maxNights="30"
-        />
-      </div>
+            <div class="box">
+                <h3>Block all dates after December 31st of the current year</h3>
+                <DatePicker :endDate="new Date(new Date().getFullYear(), 11, 31)" />
+            </div>
 
-      <div class="box">
-        <h3>Block all dates after December 31st of the current year</h3>
-        <DatePicker
-          :endDate="new Date(new Date().getFullYear(), 11, 31)"
-        />
-      </div>
+            <div class="box">
+                <h3>Block all dates after September 15th</h3>
+                <DatePicker :endDate="new Date(new Date().getFullYear(), 8, 15)" />
+            </div>
 
-      <div class="box">
-        <h3>Block all dates after September 15th</h3>
-        <DatePicker
-          :endDate="new Date(new Date().getFullYear(), 8, 15)"
-        />
-      </div>
+            <div class="box">
+                <h3>Block all date ranges of more than 30 days</h3>
+                <DatePicker :maxNights="30" :showBottomBar="false" :selectForward="false" />
+            </div>
 
-      <div class="box">
-        <h3>Block all date ranges of more than 30 days</h3>
-        <DatePicker
-          :maxNights="30"
-          :showBottomBar="false"
-          :selectForward="false"
-        />
-      </div>
+            <div class="box">
+                <h3>Minimum stay of 3 days</h3>
+                <DatePicker :minNights="3" :showBottomBar="true" />
+            </div>
 
-      <div class="box">
-        <h3>Minimum stay of 3 days</h3>
-        <DatePicker
-          :minNights="3"
-          :showBottomBar="true"
-        />
-      </div>
-
-
-      <div class="box">
-        <h3>Certain dates blocked</h3>
-        <DatePicker
-          :disabledDates="[
+            <div class="box">
+                <h3>Certain dates blocked</h3>
+                <DatePicker
+                    :disabledDates="[
               '2017-09-14',
               '2017-09-26',
           ]"
-          :showCloseButton="true"
-        />
-      </div>
+                    :showCloseButton="true"
+                />
+            </div>
 
-      <div class="box">
-        <h3>Allow setting a default date range ( can be used to set a range from a url param )</h3>
-        <DatePicker
-          :startingDateValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())"
-          :endingDateValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 5)"
-        />
-      </div>
+            <div class="box">
+                <h3>Allow setting a default date range ( can be used to set a range from a url param )</h3>
+                <DatePicker
+                    :startingDateValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())"
+                    :endingDateValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 5)"
+                />
+            </div>
 
-      <div class="box">
-        <h3>Ranges of 7, 10 or 14 days and checkin only on saturdays</h3>
-        <DatePicker
-          :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
-          :enableCheckout="true"
-          :allowedRanges="[7,10,14]"
-          :minNights="7"
-        />
-      </div>
+            <div class="box">
+                <h3>Ranges of 7, 10 or 14 days and checkin only on saturdays</h3>
+                <DatePicker
+                    :disabledDaysOfWeek="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday']"
+                    :enableCheckout="true"
+                    :allowedRanges="[7,10,14]"
+                    :minNights="7"
+                />
+            </div>
 
-      <div class="box">
-        <h3>Custom tooltip text</h3>
-        <DatePicker
-          tooltipMessage="<strong style='color: red'>Enjoy</strong> your stay!"
-        />
-      </div>
+            <div class="box">
+                <h3>Custom tooltip text</h3>
+                <DatePicker tooltipMessage="<strong style='color: red'>Enjoy</strong> your stay!" />
+            </div>
 
-      <div class="box">
-        <h3>Show year</h3>
-        <DatePicker
-          :showYear="true"
-        />
-      </div>
+            <div class="box">
+                <h3>Show year</h3>
+                <DatePicker :showYear="true" />
+            </div>
 
-      <div class="box">
-        <h3>Custom date format with i18n (e.g.: pt-PT)</h3>
-        <DatePicker
-          :minNights="0"
-          :singleDateSelection="true"
-          format="MMMM D"
-          :i18n="ptPT"
-        />
-      </div>
+            <div class="box">
+                <h3>Custom date format with i18n (e.g.: pt-PT)</h3>
+                <DatePicker
+                    :minNights="0"
+                    :singleDateSelection="true"
+                    format="MMMM D"
+                    :i18n="ptPT"
+                />
+            </div>
 
-        <div class="box">
-        <h3>Change the first day of the week to Monday</h3>
-        <DatePicker
-            :i18n="{ night: 'Night',
+            <div class="box">
+                <h3>Change the first day of the week to Monday</h3>
+                <DatePicker
+                    :i18n="{ night: 'Night',
                       nights: 'Nights',
                       'day-names': ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
                       'check-in': 'Check-in',
                       'check-out': 'Check-Out',
                       'month-names': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
                     }"
-            :firstDayOfWeek="1"
-        />
-    </div>
+                    :firstDayOfWeek="1"
+                />
+            </div>
 
-    <div class="box">
-        <h3>Set checkIn value</h3>
-        <DatePicker
-            :i18n="{ night: 'Night',
+            <div class="box">
+                <h3>Set checkIn value</h3>
+                <DatePicker
+                    :i18n="{ night: 'Night',
                       nights: 'Nights',
                       'day-names': ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
                       'check-in': 'Check-in',
                       'check-out': 'Check-Out',
                       'month-names': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
                     }"
-            :firstDayOfWeek="1"
-            :checkInValue="new Date()"
-        />
-    </div>
+                    :firstDayOfWeek="1"
+                    :checkInValue="new Date()"
+                />
+            </div>
 
-    <div class="box">
-        <h3>Set checkOut value</h3>
-        <DatePicker
-            :i18n="{ night: 'Night',
+            <div class="box">
+                <h3>Set checkOut value</h3>
+                <DatePicker
+                    :i18n="{ night: 'Night',
                       nights: 'Nights',
                       'day-names': ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
                       'check-in': 'Check-in',
                       'check-out': 'Check-Out',
                       'month-names': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
                     }"
-            :firstDayOfWeek="1"
-            :checkInValue="new Date()"
-            :checkOutValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 3)"
-        />
+                    :firstDayOfWeek="1"
+                    :checkInValue="new Date()"
+                    :checkOutValue="new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 3)"
+                />
+            </div>
+        </div>
     </div>
-
-    </div>
-  </div>
 </template>
 
 <script>
-  import DatePicker from 'components/DatePicker.vue';
+import DatePicker from './components/DatePicker.vue';
 
-  export default {
-    components: {
-      DatePicker
-    },
+export default {
+  components: {
+    DatePicker
+  },
 
-    data() {
-      return {
-        ptPT: {
-          night: 'Noite',
-          nights: 'Noites',
-          'day-names': ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-          'check-in': 'Chegada',
-          'check-out': 'Partida',
-          'month-names': ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-        },
-      };
-    }
-  };
-
+  data() {
+    return {
+      ptPT: {
+        night: 'Noite',
+        nights: 'Noites',
+        'day-names': ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+        'check-in': 'Chegada',
+        'check-out': 'Partida',
+        'month-names': [
+          'Janeiro',
+          'Fevereiro',
+          'Março',
+          'Abril',
+          'Maio',
+          'Junho',
+          'Julho',
+          'Agosto',
+          'Setembro',
+          'Outubro',
+          'Novembro',
+          'Dezembro'
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style>
-  body,
-  html {
-    font-family: 'Source Sans Pro', sans-serif;
-  }
+body,
+html {
+    font-family: "Source Sans Pro", sans-serif;
+}
 
-  .box {
+.box {
     width: 100%;
-  }
+}
 </style>
---
+++ src/helpers.js
@@ -0,0 +1,163 @@
+export default {
+  getNextDate(datesArray, referenceDate) {
+    var now = new Date(referenceDate);
+    var closest = Infinity;
+
+    datesArray.forEach(function (d) {
+      var date = new Date(d);
+      if (date >= now && date < closest) {
+        closest = d;
+      }
+    });
+
+    if (closest === Infinity) {
+      return null;
+    } else {
+      return closest;
+    }
+  },
+  nextDateByDayOfWeek(weekDay, referenceDate) {
+    referenceDate = new Date(referenceDate);
+    weekDay = weekDay.toLowerCase();
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    let referenceDateDay = referenceDate.getDay();
+
+    for (var i = 7; i--;) {
+      if (weekDay === days[i]) {
+        weekDay = (i <= referenceDateDay) ? (i + 7) : i;
+        break;
+      }
+    }
+
+    let daysUntilNext = weekDay - referenceDateDay;
+
+    return referenceDate.setDate(referenceDate.getDate() + daysUntilNext);
+  },
+  nextDateByDayOfWeekArray(daysArray, referenceDate) {
+    let tempArray = [];
+    for (var i = 0; i < daysArray.length; i++) {
+      tempArray.push(new Date(this.nextDateByDayOfWeek(daysArray[i], referenceDate)));
+    }
+    return this.getNextDate(tempArray, referenceDate);
+  },
+  isDateLessOrEquals(time1, time2) {
+    return new Date(time1) <= new Date(time2);
+  },
+  countDays(start, end) {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const firstDate = new Date(start);
+    const secondDate = new Date(end);
+
+    return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+  },
+  addDays(date, quantity) {
+    let result = new Date(date);
+    result.setDate(result.getDate() + quantity);
+    return result;
+  },
+  getFirstDay(date, firstDayOfWeek) {
+    var firstDay =  this.getFirstDayOfMonth(date);
+    var offset = 0;
+    if (firstDayOfWeek > 0) {
+      offset = firstDay.getDay() === 0 ? -7 + firstDayOfWeek : firstDayOfWeek;
+    }
+    return new Date(
+      firstDay.setDate(
+        firstDay.getDate()
+        - (firstDay.getDay() - offset)
+      )
+    );
+  },
+  getFirstDayOfMonth(date) {
+    return new Date(
+      date.getFullYear(),
+      date.getMonth(), 1
+    );
+  },
+  getNextMonth(date) {
+    let nextMonth;
+
+    if (date.getMonth() == 11) {
+      nextMonth = new Date(date.getFullYear() + 1, 0, 1);
+    } else {
+      nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    }
+    return nextMonth;
+  },
+
+  swipeAfterScroll(direction) {
+    if (this.screenSize !== 'desktop' && this.isOpen) {
+      const swiperWrapper = document.getElementById('swiperWrapper');
+
+      // If wrapper has vertical scroll
+      if (swiperWrapper.scrollHeight > swiperWrapper.clientHeight) {
+        if (swiperWrapper.scrollTop === (swiperWrapper.scrollHeight - swiperWrapper.offsetHeight)) {
+          this.renderNextMonth();
+        }
+        else if (swiperWrapper.scrollTop === 0) {
+          this.renderPreviousMonth();
+        }
+        else {
+          return;
+        }
+      }
+      else if (direction == 'up') {
+        this.renderNextMonth();
+      }
+      else if (direction == 'down') {
+        this.renderPreviousMonth();
+      }
+    }
+  },
+
+  handleTouchStart(evt) {
+    this.xDown = evt.touches[0].clientX;
+    this.yDown = evt.touches[0].clientY;
+  },
+  getMonthDiff(d1, d2) {
+    d1 = new Date(d1);
+    d2 = new Date(d2);
+    const d1Y = d1.getFullYear();
+    const d2Y = d2.getFullYear();
+    const d1M = d1.getMonth();
+    const d2M = d2.getMonth();
+    return (d2M + 12 * d2Y) - (d1M + 12 * d1Y);
+  },
+  handleTouchMove(evt) {
+    if (!this.xDown || !this.yDown) {
+      return;
+    }
+
+    this.xUp = evt.touches[0].clientX;
+    this.yUp = evt.touches[0].clientY;
+
+    let xDiff = this.xDown - this.xUp;
+    let yDiff = this.yDown - this.yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+        // swipe right
+      } else {
+        // swipe left
+      }
+    } else {
+      if (yDiff > 0) {
+        // swipe up
+        this.swipeAfterScroll('up');
+      } else {
+        // swipe down
+        this.swipeAfterScroll('down');
+      }
+    }
+    this.xDown = null;
+    this.yDown = null;
+  },
+
+  shortenString(arr, sLen) {
+    let newArr = [];
+    for (let i = 0, len = arr.length; i < len; i++) {
+      newArr.push(arr[i].substr(0, sLen));
+    }
+    return newArr;
+  },
+};
