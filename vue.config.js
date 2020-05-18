122fac017b890c79ffc53f4cbeceb37e80221313
Matias Perrone <matias@globalinnovation.com.ar>
2020-05-18 12:46:57 -0300

    v3.0.0 Final Release

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 var path = require('path');
 const VueDir = path.resolve(__dirname, 'src');
 
-console.log('VueDir: ' + VueDir);
-
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set('@', VueDir);
