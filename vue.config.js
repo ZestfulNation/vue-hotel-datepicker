cbd5365749604af0a086910792ad0d80f248bb52
Matias Perrone <matias@globalinnovation.com.ar>
2020-05-20 16:32:58 -0300

    v3.0.4 - Build changes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ var path = require('path');
 const VueDir = path.resolve(__dirname, 'src');
 
 module.exports = {
+  filenameHashing: false,
   chainWebpack: config => {
+    config.output.filename = 'vue-hotel-datepicker.min.js';
     config.resolve.alias.set('@', VueDir);
     config.resolve.alias.set('~', VueDir);
   }
