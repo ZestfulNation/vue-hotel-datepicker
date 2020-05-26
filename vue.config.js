ebcc73d5549fabded21ce1833462b7b725d29c47
Matias Perrone <matias@globalinnovation.com.ar>
2020-05-26 16:27:48 -0300

    Build UMD fix (CSS Inline)

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,9 @@ var path = require('path');
 const VueDir = path.resolve(__dirname, 'src');
 
 module.exports = {
+  css: { extract: false },
   filenameHashing: false,
   chainWebpack: config => {
-    // config.output.filename = 'vue-hotel-datepicker.min.js';
-    // config.output.libraryTarget = 'umd';
-    // config.output.library = 'HotelDatePicker';
-
     config.entry = path.resolve(__dirname, '../src/components/DatePicker.vue');
     config.devtool = '#source-map';
 
