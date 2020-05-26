b82f482b769da2f8c8d59760e753ec4e62e1248d
Matias Perrone <matias@globalinnovation.com.ar>
2020-05-26 16:14:41 -0300

    v3.0.5 - Build UMD fix

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,13 @@ const VueDir = path.resolve(__dirname, 'src');
 module.exports = {
   filenameHashing: false,
   chainWebpack: config => {
-    config.output.filename = 'vue-hotel-datepicker.min.js';
+    // config.output.filename = 'vue-hotel-datepicker.min.js';
+    // config.output.libraryTarget = 'umd';
+    // config.output.library = 'HotelDatePicker';
+
+    config.entry = path.resolve(__dirname, '../src/components/DatePicker.vue');
+    config.devtool = '#source-map';
+
     config.resolve.alias.set('@', VueDir);
     config.resolve.alias.set('~', VueDir);
   }
