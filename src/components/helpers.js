4adb151f4a4761f360aabf8c4ec7daf6c174af44
joffrey berrier <joffrey.berrier@gmail.com>
2020-06-02 19:01:11 +0200

    vue-hotel-datepicker@0.5.0-performance-mobile

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -96,20 +96,29 @@ export default {
     return nextMonth;
   },
   handleTouchStart(evt) {
-    if (this.isOpen && !this.isPreventedMaxMonth) {
+    this.isTouchMove = false;
+
+    if (this.isOpen) {
       this.xDown = evt.touches[0].clientX;
       this.yDown = evt.touches[0].clientY;
     }
   },
   handleTouchMove(evt) {
     if (!this.xDown || !this.yDown) {
+      this.isTouchMove = false;
+
       return;
     }
 
+    this.isTouchMove = true;
     this.xUp = evt.touches[0].clientX;
     this.yUp = evt.touches[0].clientY;
   },
   handleTouchEnd() {
+    if (!this.isTouchMove) {
+      return;
+    }
+
     if (!this.xDown || !this.yDown) {
       return;
     }
@@ -117,7 +126,11 @@ export default {
     const xDiff = this.xDown - this.xUp;
     const yDiff = this.yDown - this.yUp;
 
-    if (Math.abs(xDiff) < Math.abs(yDiff) && yDiff > 0) {
+    if (
+      Math.abs(xDiff) < Math.abs(yDiff) &&
+      yDiff > 0 &&
+      !this.isPreventedMaxMonth
+    ) {
       this.renderNextMonth();
     } else {
       this.renderPreviousMonth();
