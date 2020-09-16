9de513abcc694e0040991677614448a20e7066f3
Matias Perrone <matias@globalinnovation.com.ar>
2020-09-16 20:28:09 -0300

    Dependencies: version bump Vue config: Force using relative dirs

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-const path = require('path')
-
 module.exports = {
     css: { extract: true },
-    chainWebpack: config => {
-        config.resolve.alias.set('@', path.resolve(__dirname, './src'))
-        config.resolve.alias.set('~', path.resolve(__dirname, './src'))
-    },
 }
