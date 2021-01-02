e02fdc476e489ea5c6e0a86302fcbb088e7849b3
Nathan Hastings <nathan@nathanhastings.co.uk>
2021-01-02 21:53:14 +0000

    only compare nextDisabledDate if it has a value

--- src/DatePicker/components/Day.vue
+++ src/DatePicker/components/Day.vue
@@ -653,7 +653,7 @@ export default {
       }
     },
     disableNextDays() {
-      if (!this.isDateLessOrEquals(this.date, this.nextDisabledDate) && this.nextDisabledDate !== Infinity) {
+      if (this.nextDisabledDate !== null && !this.isDateLessOrEquals(this.date, this.nextDisabledDate) && this.nextDisabledDate !== Infinity) {
         this.isDisabled = true
       } else if (this.isDateLessOrEquals(this.date, new Date().setDate(this.options.startDate.getDate() - 1))) {
         this.isDisabled = true
