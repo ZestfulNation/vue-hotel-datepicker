150c6841559a27abe9f8e25b158dfecc525cac89
Matias Perrone <github@matiasperrone.com>
2021-02-12 13:28:53 -0300

    Added Price component

---
+++ src/DatePicker/components/Price.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="show && price" class="price">
+    <span class="price-symbol">{{ symbol }}</span>
+    <span class="price-number">{{ price }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Price',
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    symbol: {
+      type: [String, null, undefined],
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
